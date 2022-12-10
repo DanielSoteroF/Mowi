67599ddae5d8a320c94ad3052f1436fcda1e6c34
DanielSoteroF <119996358+DanielSoteroF@users.noreply.github.com>
2022-12-10 12:47:36 -0500

    Delete MowiTaxiApp directory

--- MowiTaxiApp/JavaScript.js
+++
@@ -1,7 +0,0 @@
-function redireccionConductor(){
-    location.href="https://play.google.com/store/apps/details?id=com.mowitunigperu.mowitaxi.mowiconductor&hl=es"
-}
-
-function redireccionPasajero(){
-    location.href="https://play.google.com/store/apps/details?id=com.mowitunigperu.mowitaxi.mowicliente&hl=es"
-}
