79dcc6ce64ed3f67edbaeb0610ecec8872dff9c6
DanielSoteroF <119996358+DanielSoteroF@users.noreply.github.com>
2022-12-10 12:31:50 -0500

    Add files via upload

---
+++ MowiTaxiApp/JavaScript.js
@@ -0,0 +1,7 @@
+function redireccionConductor(){
+    location.href="https://play.google.com/store/apps/details?id=com.mowitunigperu.mowitaxi.mowiconductor&hl=es"
+}
+
+function redireccionPasajero(){
+    location.href="https://play.google.com/store/apps/details?id=com.mowitunigperu.mowitaxi.mowicliente&hl=es"
+}
